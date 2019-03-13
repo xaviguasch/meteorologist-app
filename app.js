eeb1c35ab35c8398c17ff5211fefda82ad9fe2b0
xaviguasch <xaviguasch@gmail.com>
2019-03-13 13:24:04 +0100

    made http requests to the darksky API and parsed the response data

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const request = require('request')
+
+
+const url = 'https://api.darksky.net/forecast/463eac0b14ffa688377d10e96e63a6c0/37.8267,-122.4233'
+
+request({
+    url: url
+}, (err, response) => {
+    const data = JSON.parse(response.body)
+
+    console.log(data);
+
+
+})
