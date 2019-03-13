57e0a19b229137b3c2305445f101b92e752ffc75
xaviguasch <xaviguasch@gmail.com>
2019-03-13 13:34:57 +0100

    making it so request automatically parses the json code for us

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/463eac0b14ffa688377d10e96e63a6c0/37.8267,-122.4233'
 
 request({
-    url: url
+    url: url,
+    json: true
 }, (err, response) => {
-    const data = JSON.parse(response.body)
+    const data = response.body.currently
 
     console.log(data);
 
